260b3cbcfba8f6b7d80c4cf7c83dfeb8678b6d9f
Markku Niemi <markku.niemi@centria.fi>
2025-02-07 15:49:06 +0200

    StaticWebApp kansion lisäys + sisältö

---
+++ Week 1-2/StaticWebApp/SinglePage.js
@@ -0,0 +1,5 @@
+// Basic form submission feedback
+document.getElementById('contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Form submitted. Thank you!');
+});
